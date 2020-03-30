9ecc91a6b4a05bd0d33a18ef00b36433d2553422
Josh <joshrbarrow@gmail.com>
2020-03-30 15:17:31 -0700

    wire arrow up

--- src/Reducers/Game.js
+++ src/Reducers/Game.js
@@ -1,9 +1,20 @@
 const initialState ={
-  shipCoordinates: [0, 0],
+  shipCoordinates: [window.innerWidth/2, window.innerHeight/2],
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+
+    case "ARROW_UP":
+      return {
+        ...state,
+        shipCoordinates: [
+          state.shipCoordinates[0],
+          state.shipCoordinates[1]-2,
+        ],
+      }
+
+
     default: return state
   }
 }
--- src/Sagas/keyBindings.js
+++ src/Sagas/keyBindings.js
@@ -1,7 +1,21 @@
 import { call, put, takeLatest } from 'redux-saga/effects'
 
-function* handleKey () {
-  console.log("hi");
+function* handleKey ({ payload }) {
+  const keyPressed = payload.code
+  switch (keyPressed) {
+    case "ArrowLeft":
+      yield put({ type: "ARROW_LEFT", payload: null })
+      break
+
+    case "ArrowRight":
+      yield put({ type: "ARROW_RIGHT", payload: null })
+      break
+
+    case "ArrowUp":
+      yield put({ type: "ARROW_UP", payload: null })
+      break
+  }
+
 }
 
 export default function* () {
