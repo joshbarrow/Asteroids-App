cb620f28fe888f8e65af8340ad2ed06b409ee1c0
Josh <joshrbarrow@gmail.com>
2020-03-30 16:19:40 -0700

    add degree rotation

--- src/Components/Ship.js
+++ src/Components/Ship.js
@@ -4,17 +4,23 @@ import { connect } from 'react-redux'
 
 const mapStateToProps = state => ({
   shipCoordinates: state.game.shipCoordinates,
+  shipRotation: state.game.shipRotation,
 })
 
 const mapDispatchToProps = null
 
 const Ship = ({
   shipCoordinates,
+  shipRotation,
 }) => {
   const [ x, y ] = shipCoordinates
   return(
     <div
-      style={{ left: x, top: y, }}
+      style={{
+        left: x,
+        top: y,
+        transform:`rotate(${shipRotation}deg)`
+      }}
       id="ship">
     </div>
   )
--- src/Reducers/Game.js
+++ src/Reducers/Game.js
@@ -1,5 +1,42 @@
 const initialState ={
   shipCoordinates: [window.innerWidth/2, window.innerHeight/2],
+  shipRotation: 0,
+}
+
+const INCREMENT_AMOUNT = 20
+
+function newShipY (currentShipY, rotation) {
+  switch (rotation) {
+    case 0: return currentShipY-INCREMENT_AMOUNT
+    case 30: return currentShipY-(INCREMENT_AMOUNT*.66)
+    case 60: return currentShipY-(INCREMENT_AMOUNT*.33)
+    case 90: return currentShipY
+    case 120: return currentShipY+(INCREMENT_AMOUNT*.33)
+    case 150: return currentShipY+(INCREMENT_AMOUNT*.66)
+    case 180: return currentShipY+INCREMENT_AMOUNT
+    case 210: return currentShipY+(INCREMENT_AMOUNT*.66)
+    case 240: return currentShipY+(INCREMENT_AMOUNT*.33)
+    case 270: return currentShipY
+    case 300: return currentShipY-(INCREMENT_AMOUNT*.33)
+    case 330: return currentShipY-(INCREMENT_AMOUNT*.66)
+  }
+}
+
+function newShipX (currentShipX, rotation) {
+  switch (rotation) {
+    case 0: return currentShipX
+    case 30: return currentShipX+(INCREMENT_AMOUNT*.33)
+    case 60: return currentShipX+(INCREMENT_AMOUNT*.66)
+    case 90: return currentShipX+INCREMENT_AMOUNT
+    case 120: return currentShipX+(INCREMENT_AMOUNT*.33)
+    case 150: return currentShipX+(INCREMENT_AMOUNT*.66)
+    case 180: return currentShipX
+    case 210: return currentShipX-(INCREMENT_AMOUNT*.33)
+    case 240: return currentShipX-(INCREMENT_AMOUNT*.66)
+    case 270: return currentShipX-INCREMENT_AMOUNT
+    case 300: return currentShipX-(INCREMENT_AMOUNT*.66)
+    case 330: return currentShipX-(INCREMENT_AMOUNT*.33)
+  }
 }
 
 export default (state = initialState, action) => {
@@ -9,11 +46,22 @@ export default (state = initialState, action) => {
       return {
         ...state,
         shipCoordinates: [
-          state.shipCoordinates[0],
-          state.shipCoordinates[1]-2,
+          newShipX(state.shipCoordinates[0], state.shipRotation),
+          newShipY(state.shipCoordinates[1], state.shipRotation),
         ],
       }
 
+    case "ARROW_RIGHT":
+      return {
+        ...state,
+        shipRotation: (state.shipRotation+30 === 360) ? 0 : state.shipRotation+30,
+      }
+
+    case "ARROW_LEFT":
+      return {
+        ...state,
+        shipRotation: (state.shipRotation-30 === -30) ? 330 : state.shipRotation-30,
+      }
 
     default: return state
   }
