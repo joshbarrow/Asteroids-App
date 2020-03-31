2eb9f568b5174a39caac436823ff3c328233f008
Josh <joshrbarrow@gmail.com>
2020-03-31 14:57:17 -0700

    add x and y coordinates logic for ship

--- src/Reducers/Game.js
+++ src/Reducers/Game.js
@@ -3,40 +3,70 @@ const initialState ={
   shipRotation: 0,
 }
 
-const INCREMENT_AMOUNT = 20
+const INCREMENT_AMOUNT = 30
 
-function newShipY (currentShipY, rotation) {
-  switch (rotation) {
-    case 0: return currentShipY-INCREMENT_AMOUNT
-    case 30: return currentShipY-(INCREMENT_AMOUNT*.66)
-    case 60: return currentShipY-(INCREMENT_AMOUNT*.33)
-    case 90: return currentShipY
-    case 120: return currentShipY+(INCREMENT_AMOUNT*.33)
-    case 150: return currentShipY+(INCREMENT_AMOUNT*.66)
-    case 180: return currentShipY+INCREMENT_AMOUNT
-    case 210: return currentShipY+(INCREMENT_AMOUNT*.66)
-    case 240: return currentShipY+(INCREMENT_AMOUNT*.33)
-    case 270: return currentShipY
-    case 300: return currentShipY-(INCREMENT_AMOUNT*.33)
-    case 330: return currentShipY-(INCREMENT_AMOUNT*.66)
+function newShipX(x, rotation) {
+  const isLeftOfYAxis = rotation > 180
+  const isBelowXAxis = rotation > 90 && rotation <= 270
+
+  // since we simply want to observe this degree's rotation within it's quadrant,
+  // we can modify the original degree until it is within 90. this allows us to see
+  // how close we are to completing this quadrant (i.e. 30 is to 90 as 120 is to 180)
+  let modifiedRotation = rotation
+  while(modifiedRotation > 90) {
+    modifiedRotation = modifiedRotation - 90
   }
+
+  let value = INCREMENT_AMOUNT * (modifiedRotation / 90)
+
+  //  if we are below the x axis, we'll want to invert our percentage so that we are
+  // decrementing, rather than incrementing (i.e. .66 becomes .33)
+  if (isBelowXAxis) {
+    value = INCREMENT_AMOUNT - value
+  }
+
+  if (isLeftOfYAxis) {
+    // apply second inversion for left side of Y axis, basicaly undoing first one
+    value = INCREMENT_AMOUNT - value
+
+    // negate value (since all values left of Y axis are negative)
+    value = value * -1
+  }
+
+  return x + value
 }
 
-function newShipX (currentShipX, rotation) {
-  switch (rotation) {
-    case 0: return currentShipX
-    case 30: return currentShipX+(INCREMENT_AMOUNT*.33)
-    case 60: return currentShipX+(INCREMENT_AMOUNT*.66)
-    case 90: return currentShipX+INCREMENT_AMOUNT
-    case 120: return currentShipX+(INCREMENT_AMOUNT*.33)
-    case 150: return currentShipX+(INCREMENT_AMOUNT*.66)
-    case 180: return currentShipX
-    case 210: return currentShipX-(INCREMENT_AMOUNT*.33)
-    case 240: return currentShipX-(INCREMENT_AMOUNT*.66)
-    case 270: return currentShipX-INCREMENT_AMOUNT
-    case 300: return currentShipX-(INCREMENT_AMOUNT*.66)
-    case 330: return currentShipX-(INCREMENT_AMOUNT*.33)
+function newShipY(y, rotation) {
+  const isLeftOfYAxis = rotation > 180
+  const isBelowXAxis = rotation > 90 && rotation <= 270
+
+  // since we simply want to observe this degree's rotation within it's quadrant,
+  // we can modify the original degree until it is within 90. this allows us to see
+  // how close we are to completing this quadrant (i.e. 30 is to 90 as 120 is to 180)
+  let modifiedRotation = rotation
+  while(modifiedRotation > 90) {
+    modifiedRotation = modifiedRotation - 90
   }
+
+  // since our value is the polar opposite of newX, we subtract our value from INCREMENT_AMOUNT
+  let value = INCREMENT_AMOUNT - (INCREMENT_AMOUNT * (modifiedRotation / 90))
+
+  //  if we are below the x axis, we'll want to invert our percentage so that we are
+  // decrementing, rather than incrementing (i.e. .66 becomes .33)
+  if (isBelowXAxis && !isLeftOfYAxis) {
+    value = INCREMENT_AMOUNT - value
+  }
+
+  if (!isBelowXAxis && isLeftOfYAxis) {
+    value = INCREMENT_AMOUNT - value
+    value = value * -1
+  }
+
+  if (!isBelowXAxis && !isLeftOfYAxis) {
+    value = value * -1
+  }
+
+  return y + value
 }
 
 export default (state = initialState, action) => {
